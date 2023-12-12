b9f7fdc545f599be6813f5ad82efd960d39182c5
appyon <105517525+appyon@users.noreply.github.com>
2023-12-12 19:06:46 +0800

    Add files via upload

---
+++ 1208/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.slider').slick({
+        
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        slidesPerRow: 2,
+        autoplay: true,
+        dots: true
+    });
+});
+
